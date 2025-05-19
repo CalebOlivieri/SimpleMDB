6f62e5bf63c8c152c72ac2bf376a56776df48445
Caleb Olivieri <you@example.com>
2025-05-19 16:11:14 -0400

    HttpUtils added

---
+++ src/auth/AuthController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Net;
+using System.Text;
+
+namespace SimpleMDB;
+
+public class AuthController
+{
+    public AuthController()
+    {
+
+    }
+
+    public async Task LandingPageGet(HttpListenerRequest req, HttpListenerResponse res, Hashtable options)
+    {
+        string html = HtmlTemplates.Base("SimpleMDB", "Landing Page","Hello, World!2");
+        await HttpUtils.Respond(req, res, options, (int)HttpStatusCode.OK, html);
+    }
+}
